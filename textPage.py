36e9986817c33e2b4cf6c4307c39c204a78b1bf5
Prajwal Hukkeri <115139007+My-infotech02@users.noreply.github.com>
2023-08-11 09:45:33 +0530

    textPage.py

--- textPage.py
+++ textPage.py
@@ -63,7 +63,7 @@ def renderPage():
     st.text("")
     type = st.selectbox(
      'Type of analysis',
-     ('Positive/Negative/Neutral - TextBlob'))
+     ('Positive/Negative/Neutral - TextBlob','Happy/Sad/Angry/Fear/Surprise - text2emotion'))
     st.text("")
     if st.button('Predict'):
         if(userText!="" and type!=None):
