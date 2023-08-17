31c69d6f3b2bfc85041160489d51470015a6c29f
Prajwal Hukkeri <115139007+My-infotech02@users.noreply.github.com>
2023-08-17 13:36:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from textblob import TextBlob
-from PIL import Image
 import text2emotion as te
 import plotly.graph_objects as go
 import requests
