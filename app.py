d3c53ebbde3eefcc09abae331577c2bded17c278
Prajwal Hukkeri <115139007+My-infotech02@users.noreply.github.com>
2023-08-17 13:50:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import sidebar
 import textPage
 # import audioPage
-import main
+import pandas as pd
+from textblob import TextBlob
+
 import imagePage
 # import videoPage
 #import twitterAnalysisPage
@@ -15,7 +17,30 @@ if page=="Text":
 # elif page=="Audio":
 #     audioPage.renderPage()
 elif page=="Twitter":
-    main.renderPage()
+        st.title("Twitter Sentiment Analysis")
+    components.html("""<hr style="height:3px;border:none;color:#333;background-color:#333; margin-bottom: 10px" /> """)
+
+# Upload a CSV file containing the Twitter data
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+    if uploaded_file is not None:
+    # Load the uploaded CSV file into a DataFrame
+        data = pd.read_csv(uploaded_file)
+
+    # Analyze sentiments
+        data['Sentiment'] = data['Text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+    # Categorize sentiments
+        data['Sentiment_Category'] = data['Sentiment'].apply(lambda x: 'Positive' if x > 0 else 'Negative' if x < 0 else 'Neutral')
+
+    # Display sentiment analysis results
+        st.write("Sentiment Analysis Results:")
+        st.write(data['Sentiment_Category'].value_counts())
+
+    # Display the uploaded dataset with sentiment analysis
+        st.write("Uploaded Dataset with Sentiment Analysis:")
+        st.write(data)
+    
 elif page=="Image":
     imagePage.renderPage()
 # elif page=="Video":
