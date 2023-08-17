c2dd79cea7197ed38023549a455a751acc5ac9e3
Prajwal Hukkeri <115139007+My-infotech02@users.noreply.github.com>
2023-08-17 13:42:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,27 +10,27 @@ import pandas as pd
 
 # Streamlit App
 def renderPage():
-st.title("Twitter Sentiment Analysis")
+    st.title("Twitter Sentiment Analysis")
 
 # Upload a CSV file containing the Twitter data
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
-if uploaded_file is not None:
+    if uploaded_file is not None:
     # Load the uploaded CSV file into a DataFrame
-    data = pd.read_csv(uploaded_file)
+        data = pd.read_csv(uploaded_file)
 
     # Analyze sentiments
-    data['Sentiment'] = data['Text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        data['Sentiment'] = data['Text'].apply(lambda x: TextBlob(x).sentiment.polarity)
 
     # Categorize sentiments
-    data['Sentiment_Category'] = data['Sentiment'].apply(lambda x: 'Positive' if x > 0 else 'Negative' if x < 0 else 'Neutral')
+        data['Sentiment_Category'] = data['Sentiment'].apply(lambda x: 'Positive' if x > 0 else 'Negative' if x < 0 else 'Neutral')
 
     # Display sentiment analysis results
-    st.write("Sentiment Analysis Results:")
-    st.write(data['Sentiment_Category'].value_counts())
+        st.write("Sentiment Analysis Results:")
+        st.write(data['Sentiment_Category'].value_counts())
 
     # Display the uploaded dataset with sentiment analysis
-    st.write("Uploaded Dataset with Sentiment Analysis:")
-    st.write(data)
+        st.write("Uploaded Dataset with Sentiment Analysis:")
+        st.write(data)
 
 
